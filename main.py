660be55852b857519f2bf9466fb61d79cd79deb6
NikaKotliar <wanderer2805@gmail.com>
2021-10-24 19:23:39 +0300

    Recipes added

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-with open('recipes.txt') as file:
-    data = file.read()
-    print(type(data))
-    print(data)
+from pprint import *
 
+with open('recipes.txt', encoding='utf-8') as file:
+    result = {}
+    for recipt in file:
+        recipt_name = recipt.strip()
+        counter = int(file.readline().strip())
+        products = []
+        for item in range(counter):
+            ingridient, quantity, measuring = file.readline().split('|')
+            products.append(
+                {'ingridient': ingridient.strip(), 'quantity': quantity.strip(), 'measuring': measuring.strip()}
+            )
+        result[recipt] = products
+        file.readline()
+    pprint(result)
