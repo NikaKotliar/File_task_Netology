e39f83a1303b26771615c5fccebd518a21a1b983
NikaKotliar <wanderer2805@gmail.com>
2021-10-29 17:17:36 +0300

    Task_desided

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pprint import *
 
 with open('recipes.txt', encoding='utf-8') as file:
-    result = {}
+    cook_book = {}
     for recipt in file:
         recipt_name = recipt.strip()
         counter = int(file.readline().strip())
@@ -11,6 +11,21 @@ with open('recipes.txt', encoding='utf-8') as file:
             products.append(
                 {'ingridient': ingridient.strip(), 'quantity': quantity.strip(), 'measuring': measuring.strip()}
             )
-        result[recipt] = products
+        cook_book[recipt_name] = products
         file.readline()
-    pprint(result)
+    # pprint(cook_book)
+
+# функция которая принимает на вход блюдо и количество порций и считает количество ингридиентов
+shop_list_by_dishes = {}
+
+def get_shop_list_by_dishes(dishes, person_count):
+    for dish in dishes:
+        for recipt, item in cook_book.items():
+            if dish == recipt:
+                for ingridient in item:
+                    count = int(ingridient['quantity']) * person_count
+                    measuring = {'measuring': ingridient['measuring'], 'quantity': count}
+                    shop_list_by_dishes = {ingridient['ingridient']: measuring}
+                    print(shop_list_by_dishes)
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
